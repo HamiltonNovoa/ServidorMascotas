cf5df51666718ec5760ae860ac85d6c012388e78
Hamilton <hamilton-novoa@hotmail.com>
2021-11-25 17:36:17 -0500

    Prueba

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const mongoose = require("mongoose");
-const cors = require("cors");
+// const express = require('express');
+// const mongoose = require("mongoose");
+// const cors = require("cors");
 
-const app = express();
-const Router = require('./routes');
+// const app = express();
+// const Router = require('./routes');
 
 const PORT = process.env.PORT || 5000;
 const CONNECTION = 'mongodb+srv://admin:admin@cluster0.2fe4l.mongodb.net/Mascotas?retryWrites=true&w=majority';
 
-mongoose.connect(CONNECTION, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then((db) => console.log("DB is connected"))
-    .catch(err => console.error(err));
+// mongoose.connect(CONNECTION, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+//     .then((db) => console.log("DB is connected"))
+//     .catch(err => console.error(err));
 
-app.use(cors());
-app.use(express.json());
-app.use('/api', Router);
+// app.use(cors());
+// app.use(express.json());
+// app.use('/api', Router);
+
+app.get('/', (req, res) => {
+    res.send('Bienvenidos');
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
